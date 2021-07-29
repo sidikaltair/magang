f4c0b7beaad6309e5091d10a8d1ddf2912ecd6d4
sidikaltair <msidikf18@gmail.com>
2021-07-29 15:31:11 +0700

    kedua

---
+++ app/Controllers/Supplier.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\ModelKertas;
+
+class Supplier extends BaseController
+
+{
+    protected  $modelKertas;
+    public function __construct()
+    {
+        $this->modelKertas = new ModelKertas();
+    }
+    public function Kertas()
+    {
+        $kertas = $this->modelKertas->findAll();
+        $data = [
+            'title' => 'Kertas',
+            'jumbo' => 'Kertas',
+            'kertas' => 'kertas'
+        ];
+
+
+        return view('subbahan/Kertas', $data);
+    }
+
+
+    public function Lem()
+    { {
+            $data = [
+                'title' => 'Lem',
+                'jumbo' => 'Lem'
+            ];
+            return view('subbahan/Lem', $data);
+        }
+    }
+
+
+    public function Plastik()
+    { {
+            $data = [
+                'title' => 'Plastik',
+                'jumbo' => 'Plastik'
+            ];
+            return view('subbahan/Plastik', $data);
+        }
+    }
+
+
+    public function Tinta()
+    { {
+            $data = [
+                'title' => 'Tinta',
+                'jumbo' => 'Tinta'
+            ];
+            return view('subbahan/tinta', $data);
+        }
+    }
+}
