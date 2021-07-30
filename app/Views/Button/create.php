26a69655fd49a7151fdf271f24dbacc9c12929ec
sidikaltair <msidikf18@gmail.com>
2021-07-30 23:04:54 +0700

    fix tabel

---
+++ app/Views/Button/create.php
@@ -0,0 +1,9 @@
+<?= $this->extend('layout/bahan'); ?>
+
+<?= $this->section('content'); ?>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem autem, ullam ab et quisquam voluptatem animi numquam dolorem accusamus obcaecati aliquid itaque consectetur quaerat vel illo aspernatur laboriosam iste alias?
+
+</p>
+
+<?= $this->endSection(); ?>
